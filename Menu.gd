d201268f0bc639fbdd25a04976432190c7921188
Cathy Ly <102442157+cl2493@users.noreply.github.com>
2025-03-29 17:51:02 -0700

    Menu and Walkable screen

---
+++ Menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _on_start_button_pressed() -> void:
+	print("Button pressed, attempting to change scene")
+	var result = get_tree().change_scene_to_file("res://Scenes/KingdomSpawn.tscn")
+	if result != OK:
+		print("Error changing scene: " + str(result))
+	else:
+		print("Scene change successful")
