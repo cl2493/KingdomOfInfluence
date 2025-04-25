bd48ec401182749ad4a62a420f1d8c04ddb81a14
AaronSant1ago <ajs2369@nau.edu>
2025-04-24 17:50:01 -0700

    added capabilities to have different npc dialog based on differing NPCs and added feature so dialog stops if player walks away

---
+++ Global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+# Global.gd
+var current_npc: Node = null
--- Scenes/npc.gd
+++ Scenes/npc.gd
@@ -7,28 +7,41 @@ func _ready():
 	connect("body_entered", _on_body_entered)
 	connect("body_exited", _on_body_exited)
 
-
 var inside = false
 var bodyName
 func _on_body_entered(body):
 	bodyName = body.name
-	print(body.name, " is in front of npc")
 	if (bodyName == "Player"):
+		get_node("/root/Global").current_npc = self
+		Dialogic.paused = false
 		inside = true
+		print(body.name, " is in front of ", self.name)
 
 func _on_body_exited(body):
 	print(body.name, " is not in front of npc")
 	if (bodyName == "Player"):
+		Dialogic.paused = true
+		Dialogic.clear()
+		get_node("/root/Global").current_npc = null
 		inside = false
 	
 func _process(delta: float) -> void:
 	if inside:
 		if Input.is_action_just_pressed("interact"):
 			print("interacted")
-			randomDialog()
-			springArm.spring_length = 0
-			teleport_player_relative_to_object(Vector3(0, 0, -1))
+			determineNPCDialog()
 			
+			#springArm.spring_length = 0
+			#teleport_player_relative_to_object(Vector3(0, 0, -1))
+			
+func determineNPCDialog():
+	match Global.current_npc.name:
+		"DrunkardNPC":
+			randomDialog()
+		"FarmerNPC":
+			print("We farming")
+		_:
+			print("TODO")
 # Function to teleport player relative to target object
 func teleport_player_relative_to_object(offset: Vector3):
 	# Get the current position of the target object
@@ -37,8 +50,8 @@ func teleport_player_relative_to_object(offset: Vector3):
 	
 	# commented because they were causing me errors
 	# Set the player's position relative to the target
-	player.global_position = position + offset
-	print("New Player Position: ", player.global_position)
+	#player.global_position = position + offset
+	#print("New Player Position: ", player.global_position)
 
 #dialog functions for village drunk
 func randomDialog():
@@ -48,3 +61,4 @@ func randomDialog():
 	
 func ended_dialog():
 	Dialogic.timeline_ended.disconnect(ended_dialog)
+	
